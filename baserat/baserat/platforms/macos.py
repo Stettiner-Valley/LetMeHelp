a0547228c8a0269ed5f80c885f745ea7937bef78
Maqbool ur Rahim Khan <maqboolurrahim@gmail.com>
2024-09-28 16:21:04 +0200

    Introduce get installed apps function, send installed apps as system prompt

--- baserat/baserat/platforms/macos.py
+++ baserat/baserat/platforms/macos.py
@@ -1,5 +1,5 @@
+import os
 from AppKit import NSWorkspace
-
 
 
 class MacOSManager():
@@ -12,3 +12,13 @@ class MacOSManager():
         # Extract and return the names of the open applications
         open_app_names = [app.localizedName() for app in running_apps]
         return open_app_names
+
+    def list_installed_apps(self):
+        app_dirs = ["/Applications", "/System/Applications", os.path.expanduser("~/Applications")]
+        installed_apps = []
+        for app_dir in app_dirs:
+            if os.path.exists(app_dir):
+                apps = [f for f in os.listdir(app_dir) if f.endswith(".app")]
+                installed_apps.extend(apps)
+
+        return installed_apps
